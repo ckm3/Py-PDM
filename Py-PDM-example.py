441c6ca2a33c938410ddde60a1ee7ba530d08322
Kaiming Cui <ckm3@nao.cas.cn>
2020-10-09 14:02:23 -0400

    Generate an example figure

--- Py-PDM-example.py
+++ Py-PDM-example.py
@@ -5,14 +5,21 @@ import matplotlib.pyplot as plt
 
 t = np.linspace(1, 20, 200)
 y = np.sin(t)
-s = np.zeros(200)
+s = np.zeros(t.size)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, constrained_layout=True)
+
+ax1.plot(t, y, 'k.')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Mag')
 
 freq, theta = pypdm.pdm(200, t, y, s, 0.01, 1, 0.001)
 
-plt.plot(freq, theta, 'k')
-plt.axvline(1/2/np.pi, color='red')
+ax2.plot(freq, theta, 'k')
+ax2.axvline(1/2/np.pi, color='red')
 for i in range(2,4):
-    plt.axvline(1/2/i/np.pi, color='red', ls='--')
-plt.xlabel('Frequency')
-plt.ylabel('Theta')
-plt.show()
+    ax2.axvline(1/2/i/np.pi, color='red', ls='--')
+ax2.set_xlabel('Frequency')
+ax2.set_ylabel('Theta')
+# plt.show()
+plt.savefig('Py-PDM-example.png')
