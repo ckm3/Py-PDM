7b6a799cc85b86ded91dab794fcb54e5ea2b6669
ckm3 <ckm@nao.cas.cn>
2020-10-10 10:37:32 -0400

    Add hard copys to keep the inputs

--- Py-PDM.pyx
+++ Py-PDM.pyx
@@ -14,11 +14,20 @@ cdef extern from "PyPDM.c":
 
 
 def pdm(int data_number, np.ndarray x , np.ndarray y, np.ndarray s, double f_min, double f_max, double delf):
-    cdef double [:] x2 = x
-    cdef double [:] y2 = y
-    cdef double [:] s2 = s
+    x_copy = x.copy()
+    y_copy = y.copy()
+    s_copy = s.copy()
 
-    pdm2(data_number, &x2[0], &y2[0], &s2[0], f_min, f_max, delf)
+    cdef double [:] x2 = x_copy
+    cdef double [:] y2 = y_copy
+    cdef double [:] s2 = s_copy
+
+    return_code = pdm2(data_number, &x2[0], &y2[0], &s2[0], f_min, f_max, delf)
+
+    if return_code == 1:
+        pass
+    elif return_code == -1:
+        raise ValueError('pdm: too few points, 100 points at least.')
 
     freq_np_array = np.asarray(<double[:nf]> f_array)
     theta_np_array = np.asarray(<double[:nf]> theta_array)
